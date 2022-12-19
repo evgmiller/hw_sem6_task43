f90e67e78ce92cdb0a5eb9bc42ed0f4f2c265e35
Anna <nusiksuper@gmail.com>
2022-12-19 16:47:43 +0300

    решение 43 через метод

--- Program.cs
+++ Program.cs
@@ -11,7 +11,13 @@ double b2 =  double.Parse(Console.ReadLine()!);
 Console.Write("Введите k2: ");
 double k2 =  double.Parse(Console.ReadLine()!);
 
-double x= (b2-b1)/(k1-k2);
-double y= k1*x+b1;
+void SearchPoint (double b1, double k1, double b2, double k2)
+{
+    double x= (b2-b1)/(k1-k2);
+    double y= k1*x+b1;
+    Console.WriteLine($"({x}; {y})");
+}
+SearchPoint(b1,k1,b2,k2);
 
-Console.WriteLine($"({x}; {y})");
+
+//Console.WriteLine($"({x}; {y})");
